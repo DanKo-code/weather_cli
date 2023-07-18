c33146847691ddd2c3754bcacec71c89ae671215
Danila Kozlyakovsky <danik2003globin@gmail.com>
2023-07-19 01:02:06 +0300

    console output)

---
+++ services/log.service.js
@@ -0,0 +1,23 @@
+import chalk from 'chalk'
+import dedent from 'dedent-js'
+
+const printError = (error) => {
+    console.log(chalk.bgRed(' ERROR ') + ' ' + error)
+}
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen(' SUCCESS ') + ' ' + error)
+}
+
+const printHelp = () => {
+    console.log(
+        dedent(`${chalk.bgCyan(' HELP ')}
+        Without params - weather output
+        -s [CITY] set city
+        -h help output
+        -t [API_KEY] save token
+        `)
+    )
+}
+
+export { printError, printSuccess, printHelp }
